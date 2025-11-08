d8c564eb5dbdd38125b13b1e3c157a9ca1a96ace
internalasuransi <asuransiinternal@gmail.com>
2025-11-09 01:07:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const fileInput = document.getElementById("file-input");
+const preview = document.getElementById("preview");
+
+fileInput.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+      preview.style.display = "block";
+    };
+    reader.readAsDataURL(file);
+  } else {
+    preview.style.display = "none";
+    preview.src = "";
+  }
+});
